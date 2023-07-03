42cdc3bf5ee9b354549900aa8c4aa93f6ec14299
Arrohim Dwi Ksatria <76282186+dwiadisa@users.noreply.github.com>
2023-07-03 08:21:49 +0700

    penambahan form login

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
-<html>
+<!-- <html>
   <title>LOGIN PAGE</title>
   <body>
-    <form>
+    
+  
+
+
+
   </body>
+</html> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Page</title>
+</head>
+
+<body>
+
+  <form action="act.php" method="post">
+    <label for="username">username</label>
+    <input type="text" name="username" id="username">
+    <input type="password" name="password" id="password">
+    <button type="submit">Login</button>
+
+  </form>
+
+</body>
+
 </html>
